33b22300a126e661286fc0712d84d2b0e95db405
TelloDMan <80420517+TelloDMan@users.noreply.github.com>
2024-03-09 11:36:38 +0200

    Update auto_snmp.py

--- auto_snmp.py
+++ auto_snmp.py
@@ -5,7 +5,7 @@ community = ""  #YOUR #COMMUNITY #STRING
 Target = "" #YOUR TARGET DEVICE IP # 192.168.1.1
 
 def start_snmp():
-	#snmpwalk into the CI Router Policers
+	#snmpwalk into the PE Router Policers
 	content = subprocess.run(["snmpwalk" ,"-v2c", "-c", community ,Target+":161", "1.3.6.1.4.1.2636.3.5.2.1.7"],capture_output=True)
 
 	#split content
